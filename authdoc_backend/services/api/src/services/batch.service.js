00abfdbe885296f0828eace4e4bf461d1ee99e0d
naveencmy <you@example.com>
2026-05-02 15:07:58 +0530

    Commit the Backend with the completed  modules all the OCR/ WORKER/ API of  AuthDoc_V1 (MVP)

--- authdoc_backend/services/api/src/services/batch.service.js
+++ authdoc_backend/services/api/src/services/batch.service.js
@@ -1,4 +1,3 @@
-// batch.service.js
 import fs from "fs"
 import path from "path"
 import { BatchRepository } from "../repositories/batch.repository.js"
@@ -7,18 +6,32 @@ import { getBatchStoragePath } from "../utils/file.utils.js"
 
 export class BatchService {
   static async createBatch(data, zipPath) {
+    try {
+      const batch = await BatchRepository.createBatch(data)
 
-    const batch = await BatchRepository.createBatch(data)
-    const batchDir = getBatchStoragePath(batch.batch_id)
-    const newZipPath = path.join(batchDir, "input.zip")
-    fs.renameSync(zipPath, newZipPath)
-    await addZipJob({
-      batchId: batch.batch_id,
-      zipPath: newZipPath
-    })
-    return batch
-  }catch (error) {
-    console.error("[BATCH] ERROR:", error)
-    throw error
+      const batchDir = getBatchStoragePath(batch.batch_id)
+      const newZipPath = path.join(batchDir, "input.zip")
+
+      fs.renameSync(zipPath, newZipPath)
+
+      await addZipJob({
+        batchId: batch.batch_id,
+        zipPath: newZipPath,
+      })
+
+      return batch
+    } catch (error) {
+      console.error("[BATCH] ERROR:", error)
+      throw error
     }
   }
+
+  static async listBatches() {
+    try {
+      return await BatchRepository.listBatches()
+    } catch (error) {
+      console.error("[BATCH LIST] ERROR:", error)
+      throw error
+    }
+  }
+}
